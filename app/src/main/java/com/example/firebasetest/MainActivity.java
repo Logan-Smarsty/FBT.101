5516bc143a863ffc18f904ad098f8f52a48d851f
Logan-Smarsty <LCSmarsty@student.fullsail.edu>
2023-07-20 18:44:37 -0400

    implemented google

--- app/src/main/java/com/example/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/firebasetest/MainActivity.java
@@ -31,7 +31,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-
+import android.os.Bundle;
 
 
 //sssss
